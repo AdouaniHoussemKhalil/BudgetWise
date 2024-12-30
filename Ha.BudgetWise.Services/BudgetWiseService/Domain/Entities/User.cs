51d8a0692e91f1b53d8098006758b8e9cc244a52
Adouani Houssem <houssemadouani09@gmail.com>
2024-12-30 15:52:31 +0100

    push develop branch / project entites v1

---
+++ Ha.BudgetWise.Services/BudgetWiseService/Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public User()
+        {
+            Purchases = new HashSet<Purchase>();
+        }
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public Gender Gender { get; set; }
+        public string? Email { get; set; }
+        public ICollection<Purchase> Purchases { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastUpdatedOn { get; set; }
+    }
+}
